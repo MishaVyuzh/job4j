9210810afda3b37477ac68e0aee230caae062ad7
MishaVyuzh <vyuzzzh@yandex.ru>
2019-08-01 19:43:20 +0300

    add folder chapter_001, change README.md

---
+++ chapter_001/src/main/java/ru/job4j/calculate/Calculate.java
@@ -0,0 +1,25 @@
+package ru.job4j.calculate;
+
+/**
+ * Calculate.
+ *
+ * @author Mikhail Vyuzhanin (vyuzzzh@yandex.ru)
+ */
+public class Calculate {
+    /**
+     * Main.
+     * @param args - args.
+     */
+    public static void main(String[] args) {
+        System.out.println("Hello, Job4j.");
+    }
+
+    /**
+     * Method echo.
+     * @param name Your name.
+     * @return Echo plus your name.
+     */
+    public String echo(String name) {
+        return "Echo, echo, echo : " + name;
+    }
+}
---
+++ chapter_001/src/test/java/ru/job4j/calculate/package-info.java
@@ -0,0 +1,8 @@
+/**
+ * Package for calculate task.
+ *
+ * @author Mikhail Vyuzhanin (vyuzzzh@yandex.ru)
+ * @version $Id$
+ * @since 0.1
+ */
+package ru.job4j.calculate;
