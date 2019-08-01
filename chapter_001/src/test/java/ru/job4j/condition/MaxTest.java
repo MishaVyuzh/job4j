5b71045e803e2d49ee212b231afbcb993e685806
MishaVyuzh <vyuzzzh@yandex.ru>
2019-08-01 21:56:52 +0300

    add Max.java and MaxTest.java

---
+++ chapter_001/src/test/java/ru/job4j/condition/MaxTest.java
@@ -0,0 +1,26 @@
+package ru.job4j.condition;
+
+import org.junit.Test;
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
+
+public class MaxTest {
+    @Test
+    public void whenMax1To2Then2() {
+        Max max = new Max();
+        int result = max.max(1, 2);
+        assertThat(result, is(2));
+    }
+    @Test
+    public void whenMax2ToThen1() {
+        Max max = new Max();
+        int result = max.max(5, 2);
+        assertThat(result, is(5));
+    }
+    @Test
+    public void whenMax1EqualThen2() {
+        Max max = new Max();
+        int result = max.max(7, 7);
+        assertThat(result, is(7));
+    }
+}
