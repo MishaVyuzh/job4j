e1ee2f776f706f02b03ca6c0d69736d3e3f454ce
MishaVyuzh <vyuzzzh@yandex.ru>
2019-08-01 22:06:38 +0300

    add title Max and MaxTest

--- chapter_001/src/main/java/ru/job4j/condition/Max.java
+++ chapter_001/src/main/java/ru/job4j/condition/Max.java
@@ -1,6 +1,18 @@
 package ru.job4j.condition;
 
+/**
+ * @author Mikhail Vyuzhanin (vyuzzzh@yandex.ru)
+ * @version $Id$
+ * @since 0.1
+ */
+
 public class Max {
+    /**
+     * Вывод максимума из 2-х значений.
+     * @param left первое значение.
+     * @param right второе значение.
+     * @return максимум.
+     */
     public int max(int left, int right) {
         return left > right ? left : right;
     }
