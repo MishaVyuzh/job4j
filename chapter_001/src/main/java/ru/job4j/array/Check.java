e62621d02f4f3ff63c0daa4b740446f38f9055f8
MishaVyuzh <vyuzzzh@yandex.ru>
2019-08-04 20:42:50 +0300

    modified Check.java

--- chapter_001/src/main/java/ru/job4j/array/Check.java
+++ chapter_001/src/main/java/ru/job4j/array/Check.java
@@ -17,10 +17,9 @@ public class Check {
     public boolean mono(boolean[] data) {
         boolean result = true;
         for (int index = 0; index != data.length; index++) {
-            if (data[0] == data[index]) {
-            }
-            else {
+            if (data[0] != data[index]) {
                 result = false;
+                break;
             }
         }
         return result;
