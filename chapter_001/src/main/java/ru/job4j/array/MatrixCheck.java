fe0e66f796b9436ee8f3f4c14a2ba0ec35e05219
MishaVyuzh <vyuzzzh@yandex.ru>
2019-08-05 23:56:34 +0300

    fix MatrixCheck

--- chapter_001/src/main/java/ru/job4j/array/MatrixCheck.java
+++ chapter_001/src/main/java/ru/job4j/array/MatrixCheck.java
@@ -17,7 +17,7 @@ public class MatrixCheck {
         boolean result = true;
 
         for (int i = 0; i < data.length; i++) {
-            if (data[0][0] != data[i][i] || data[0][0] != data[data.length - 1 - i][i]) {
+            if (data[0][0] != data[i][i] || data[0][data.length - 1] != data[data.length - 1 - i][i]) {
                 result = false;
                 break;
             }
--- chapter_001/src/test/java/ru/job4j/array/MatrixCheckTest.java
+++ chapter_001/src/test/java/ru/job4j/array/MatrixCheckTest.java
@@ -56,4 +56,14 @@ public class MatrixCheckTest {
         boolean result = check.mono(input);
         assertThat(result, is(false));
     }
+    @Test
+    public void whenDataTwoMonoFalseXTrueThenTrue() {
+        MatrixCheck check = new MatrixCheck();
+        boolean[][] input = new boolean[][] {
+                {true, false},
+                {false, true}
+        };
+        boolean result = check.mono(input);
+        assertThat(result, is(true));
+    }
 }
