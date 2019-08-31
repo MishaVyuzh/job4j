7f7ba82d3818d5fabdf251ab60afbcff9cea6066
MishaVyuzh <vyuzzzh@yandex.ru>
2019-08-31 22:17:16 +0300

    Обеспечить бесперебойную работу приложения Tracker.[#164560]

--- chapter_002/src/main/java/ru/job4j/tracker/Input.java
+++ chapter_002/src/main/java/ru/job4j/tracker/Input.java
@@ -1,5 +1,7 @@
 package ru.job4j.tracker;
 
+import java.util.List;
+
 /**
  * @author Mikhail Vyuzhanin (vyuzzzh@yandex.ru)
  * @version $#16439$
@@ -8,4 +10,5 @@ package ru.job4j.tracker;
 
 public interface Input {
     String ask(String question);
+    int ask(String question, List<Integer> range);
 }
