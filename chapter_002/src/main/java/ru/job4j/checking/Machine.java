6acd153d985428fd629d2fce2cecd3c578af62fb
MishaVyuzh <vyuzzzh@yandex.ru>
2019-10-17 14:30:21 +0300

    change in the coffee machine

---
+++ chapter_002/src/main/java/ru/job4j/checking/Machine.java
@@ -0,0 +1,22 @@
+package ru.job4j.checking;
+
+import java.util.Arrays;
+
+public class Machine {
+    private final int[] COINS = {10, 5, 2, 1};
+
+    public int[] change(int money, int price) {
+        int[] rsl = new int[100];
+        int size = 0;
+        money -= price;
+
+        for(int i = 0; i < COINS.length; i++) {
+            while(money >= COINS[i]) {
+                money -= COINS[i];
+                rsl[size] = COINS[i];
+                size++;
+            }
+        }
+        return Arrays.copyOf(rsl, size);
+    }
+}
