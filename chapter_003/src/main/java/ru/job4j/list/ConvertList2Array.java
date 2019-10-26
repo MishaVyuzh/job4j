4bfe4b04de834e70f22671f769ddf3bfc667730a
MishaVyuzh <vyuzzzh@yandex.ru>
2019-10-26 23:49:43 +0300

    change on foreach

--- chapter_003/src/main/java/ru/job4j/list/ConvertList2Array.java
+++ chapter_003/src/main/java/ru/job4j/list/ConvertList2Array.java
@@ -5,16 +5,16 @@ import java.util.List;
 public class ConvertList2Array {
     public int[][] toArray(List<Integer> list, int rows) {
         int[][] array = new int[rows][rows];
-        int toZero = 0;
+        int col = 0;
+        int str = 0;
 
-        for (int i = 0; i < rows; i++) {
-            for (int j = 0; j < rows; j++, toZero++) {
-                if (toZero < list.size()) {
-                    array[i][j] = list.get(toZero);
-                } else {
-                    array[i][j] = 0;
-                }
+        for (Integer i : list) {
+            if (str == rows) {
+                str = 0;
+                col++;
             }
+            array[col][str] = i;
+            str++;
         }
         return array;
     }
