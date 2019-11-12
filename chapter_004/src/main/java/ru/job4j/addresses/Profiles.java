cc89610f11a7462ca8a4b643640e5ece3d7c82fc
MishaVyuzh <vyuzzzh@yandex.ru>
2019-11-12 20:17:53 +0300

    2. Список адресов[#164411]

---
+++ chapter_004/src/main/java/ru/job4j/addresses/Profiles.java
@@ -0,0 +1,17 @@
+package ru.job4j.addresses;
+
+import java.util.Comparator;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class Profiles {
+    List<Address> collect(List<Profile> profiles) {
+        return profiles.stream()
+                .map(Profile::getAddress)
+                .sorted(Comparator.comparing(Address::getCity)
+                        .thenComparing(Address::getStreet)
+                        .thenComparing(Address::getHome)
+                        .thenComparing(Address::getApartment))
+                .collect(Collectors.toList());
+    }
+}
