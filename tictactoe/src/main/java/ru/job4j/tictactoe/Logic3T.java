d9726d2731d11a9d09ceaf535f8b7acbde50c922
MishaVyuzh <vyuzzzh@yandex.ru>
2019-11-22 20:47:33 +0300

    add searchFill method

--- tictactoe/src/main/java/ru/job4j/tictactoe/Logic3T.java
+++ tictactoe/src/main/java/ru/job4j/tictactoe/Logic3T.java
@@ -2,10 +2,10 @@ package ru.job4j.tictactoe;
 
 import java.util.function.Predicate;
 
-public class Logic3T {
+class Logic3T {
     private final Figure3T[][] table;
 
-    public Logic3T(Figure3T[][] table) {
+    Logic3T(Figure3T[][] table) {
         this.table = table;
     }
 
@@ -13,13 +13,13 @@ public class Logic3T {
      * Проверяем линию, заданную прараметрами, есть ли там нужные фишки.
      *
      * @param predicate подставляем метод из Figure.
-     * @param startX начало по Х проверяемой цепочки.
-     * @param startY начало по Y проверяемой цепочки.
-     * @param deltaX приращение по X.
-     * @param deltaY приращение по Y.
+     * @param startX    начало по Х проверяемой цепочки.
+     * @param startY    начало по Y проверяемой цепочки.
+     * @param deltaX    приращение по X.
+     * @param deltaY    приращение по Y.
      * @return если линия заполнена одинаковыми фигурами - true.
      */
-    public boolean fillBy(Predicate<Figure3T> predicate, int startX, int startY, int deltaX, int deltaY) {
+    private boolean fillBy(Predicate<Figure3T> predicate, int startX, int startY, int deltaX, int deltaY) {
         boolean result = true;
         for (int index = 0; index != this.table.length; index++) {
             Figure3T cell = this.table[startX][startY];
@@ -34,19 +34,30 @@ public class Logic3T {
     }
 
     /**
+     * Метод ищет заполненную линию.
+     *
+     * @return если линия найдена - true.
+     */
+    private boolean searchFill(Predicate<Figure3T> predicate) {
+        boolean result = false;
+        for (int pos = 0; pos < table.length; pos++) {
+            if (fillBy(predicate, pos, 0, 0, 1)
+                    || fillBy(predicate, 0, pos, 1, 0)
+                    || fillBy(predicate, 0, 0, 1, 1)
+                    || fillBy(predicate, 2, 0, -1, 1)) {
+                result = true;
+            }
+        }
+        return result;
+    }
+
+    /**
      * Метод проверяет есть ли в поле выигрышные комбинации для Крестика.
      *
      * @return если комбинация найдена - true.
      */
-    public boolean isWinnerX() {
-        return this.fillBy(Figure3T::hasMarkX, 0, 0, 0, 1)
-                || this.fillBy(Figure3T::hasMarkX, 1, 0, 0, 1)
-                || this.fillBy(Figure3T::hasMarkX, 2, 0, 0, 1)
-                || this.fillBy(Figure3T::hasMarkX, 0, 0, 1, 0)
-                || this.fillBy(Figure3T::hasMarkX, 0, 1, 1, 0)
-                || this.fillBy(Figure3T::hasMarkX, 0, 2, 1, 0)
-                || this.fillBy(Figure3T::hasMarkX, 0, 0, 1, 1)
-                || this.fillBy(Figure3T::hasMarkX, 2, 0, -1, 1);
+    boolean isWinnerX() {
+        return searchFill(Figure3T::hasMarkX);
     }
 
     /**
@@ -54,15 +65,8 @@ public class Logic3T {
      *
      * @return если комбинация найдена - true.
      */
-    public boolean isWinnerO() {
-        return this.fillBy(Figure3T::hasMarkO, 0, 0, 0, 1)
-                || this.fillBy(Figure3T::hasMarkO, 1, 0, 0, 1)
-                || this.fillBy(Figure3T::hasMarkO, 2, 0, 0, 1)
-                || this.fillBy(Figure3T::hasMarkO, 0, 0, 1, 0)
-                || this.fillBy(Figure3T::hasMarkO, 0, 1, 1, 0)
-                || this.fillBy(Figure3T::hasMarkO, 0, 2, 1, 0)
-                || this.fillBy(Figure3T::hasMarkO, 0, 0, 1, 1)
-                || this.fillBy(Figure3T::hasMarkO, 2, 0, -1, 1);
+    boolean isWinnerO() {
+        return searchFill(Figure3T::hasMarkO);
     }
 
     /**
@@ -70,7 +74,7 @@ public class Logic3T {
      *
      * @return если поле заполнено - false.
      */
-    public boolean hasGap() {
+    boolean hasGap() {
         boolean result = false;
         for (Figure3T[] figure : table) {
             for (int j = 0; j < table.length; j++) {
