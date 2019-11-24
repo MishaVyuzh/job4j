3121cb5adf8d4bd712784d368175cd43ad8dbdcf
MishaVyuzh <vyuzzzh@yandex.ru>
2019-11-25 02:38:31 +0300

    5.1.2. Создать итератор четные числа[#201540]

---
+++ chapter_005/src/main/java/ru/job4j/iterator/EvenIterator.java
@@ -0,0 +1,40 @@
+package ru.job4j.iterator;
+
+import java.util.Iterator;
+import java.util.NoSuchElementException;
+
+public class EvenIterator implements Iterator {
+    private final int[] numbers;
+    private int index;
+
+    public EvenIterator(final int[] numbers) {
+        this.numbers = numbers;
+        index = 0;
+    }
+
+    private boolean checkEven(int element) {
+        return element % 2 == 0;
+    }
+
+    @Override
+    public boolean hasNext() {
+        boolean result = false;
+        while (index < numbers.length) {
+            if (checkEven(numbers[index])) {
+                result = true;
+                break;
+            }
+            index++;
+        }
+        return result;
+    }
+
+    @Override
+    public Object next() {
+        if (!hasNext()) {
+            throw new NoSuchElementException("The array does not contain even numbers.");
+        }
+
+        return numbers[index++];
+    }
+}
---
+++ chapter_005/src/test/java/ru/job4j/iterator/EvenIteratorTest.java
@@ -0,0 +1,78 @@
+package ru.job4j.iterator;
+
+import java.util.Iterator;
+import java.util.NoSuchElementException;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.is;
+
+
+public class EvenIteratorTest {
+
+    private Iterator<Integer> it;
+
+    @Before
+    public void setUp() {
+        it = new EvenIterator(new int[]{1, 2, 3, 4, 5, 6, 7});
+    }
+
+    @Test(expected = NoSuchElementException.class)
+    public void shouldReturnEvenNumbersSequentially() {
+        assertThat(it.hasNext(), is(true));
+        assertThat(it.next(), is(2));
+        assertThat(it.hasNext(), is(true));
+        assertThat(it.next(), is(4));
+        assertThat(it.hasNext(), is(true));
+        assertThat(it.next(), is(6));
+        assertThat(it.hasNext(), is(false));
+        it.next();
+    }
+
+    @Test
+    public void sequentialHasNextInvocationDoesntAffectRetrievalOrder() {
+        assertThat(it.hasNext(), is(true));
+        assertThat(it.hasNext(), is(true));
+        assertThat(it.next(), is(2));
+        assertThat(it.next(), is(4));
+        assertThat(it.next(), is(6));
+    }
+
+    @Test
+    public void shouldReturnFalseIfNoAnyEvenNumbers() {
+        it = new EvenIterator(new int[]{1});
+        assertThat(it.hasNext(), is(false));
+    }
+
+    @Test
+    public void allNumbersAreEven() {
+        it = new EvenIterator(new int[]{2, 4, 6, 8});
+        assertThat(it.hasNext(), is(true));
+        assertThat(it.next(), is(2));
+        assertThat(it.hasNext(), is(true));
+        assertThat(it.next(), is(4));
+        assertThat(it.hasNext(), is(true));
+        assertThat(it.next(), is(6));
+        assertThat(it.hasNext(), is(true));
+        assertThat(it.next(), is(8));
+    }
+
+    @Test(expected = NoSuchElementException.class)
+    public void emptyArray() {
+        it = new EvenIterator(new int[]{});
+        assertThat(it.hasNext(), is(false));
+        it.next();
+    }
+
+    @Test
+    public void whenContainsOneEvenElement() {
+        it = new EvenIterator(new int[]{1, 3, 5, 7, 22});
+        assertThat(it.hasNext(), is(true));
+        assertThat(it.hasNext(), is(true));
+        assertThat(it.next(), is(22));
+        assertThat(it.hasNext(), is(false));
+    }
+}
+
